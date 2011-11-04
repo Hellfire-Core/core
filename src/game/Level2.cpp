6eb77c03d1e1fb52b60d3541e13e14c38cc0bc19
Anomandaris <Anomandaris@localhost>
2011-11-04 17:28:29 +0100

    Fix .npc addformation follow angle.

--- src/game/Level2.cpp
+++ src/game/Level2.cpp
@@ -4076,6 +4076,8 @@ bool ChatHandler::HandleNpcAddFormationCommand(const char* args)
 
     group_member                 = new FormationInfo;
     group_member->follow_angle   = pCreature->GetAngle(chr) - chr->GetOrientation();
+    if(group_member->follow_angle < 0)
+        group_member->follow_angle += 2 * M_PI;
     group_member->follow_dist    = sqrtf(pow(chr->GetPositionX() - pCreature->GetPositionX(),int(2))+pow(chr->GetPositionY()-pCreature->GetPositionY(),int(2)));
     group_member->leaderGUID     = leaderGUID;
     group_member->groupAI        = gAI;
