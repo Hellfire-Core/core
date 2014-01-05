69599cbeed2e8c75bc899996e9f0aab7254f4b69
Siarkowy <siarkowy@siarkowy.net>
2014-01-06 00:59:14 +0100

    Instances/Zul'Aman: Fix Nalorakk positioning after wipe

--- src/scripts/scripts/zone/zulaman/boss_nalorakk.cpp
+++ src/scripts/scripts/zone/zulaman/boss_nalorakk.cpp
@@ -332,6 +332,7 @@ struct boss_nalorakkAI : public ScriptedAI
                     return;
                 case 7:
                     m_creature->SetOrientation(3.1415*0.5);
+                    m_creature->SetHomePosition(NalorakkWay[7][0], NalorakkWay[7][1], NalorakkWay[7][2], m_creature->GetOrientation()); 
                     inMove = false;
                     return;
             }
