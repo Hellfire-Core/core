37bb614a4cf70c19d381929bc7be14395763a2d5
Makwell <Makwell@localhost>
2014-02-18 22:19:37 +0100

    Shadowsword Guardian: fix Earthquake;

--- src/scripts/scripts/zone/sunwell_plateau/sunwell_plateau_trash.cpp
+++ src/scripts/scripts/zone/sunwell_plateau/sunwell_plateau_trash.cpp
@@ -2256,7 +2256,7 @@ CreatureAI* GetAI_mob_priestess_of_torment(Creature *_Creature)
 enum ShadowswordGuardian
 {
     SPELL_BEAR_DOWN                 = 46239,
-    SPELL_EARTHQUAKE                = 46240
+    SPELL_EARTHQUAKE                = 46932
 };
 
 struct mob_shadowsword_guardianAI : public ScriptedAI
@@ -2285,6 +2285,8 @@ struct mob_shadowsword_guardianAI : public ScriptedAI
 
     void EnterCombat(Unit*)
     {
+        if (pInstance->GetData(DATA_MURU_EVENT) != DONE)
+            EnterEvadeMode();
         DoCast(me, SPELL_EARTHQUAKE);
         DoZoneInCombat(80.0f);
     }
