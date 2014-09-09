20e1fa32dd7f4f45939131e8bff90670f6dcdefa
Elyrion <elyrion.hg@gmail.com>
2014-09-09 19:18:07 +0200

    koostosh: fixu fixu crashfixu

--- src/hellgroundcore/WorldRunnable.cpp
+++ src/hellgroundcore/WorldRunnable.cpp
@@ -77,7 +77,7 @@ void WorldRunnable::run()
 
     // unload battleground templates before different singletons destroyed
     sBattleGroundMgr.DeleteAllBattleGrounds();
-    sInstanceSaveManager.UnbindBeforeDelete();
+    //sInstanceSaveManager.UnbindBeforeDelete();
 
     sWorldSocketMgr->StopNetwork();
     sMapMgr.UnloadAll();                                    // unload all grids (including locked in memory)
