a105a64b0872a00e32cbd5e2f9c1614b877511ba
Elyrion <elyrion.hg@gmail.com>
2014-09-09 18:58:27 +0200

    More creature movement by anonymous

--- src/game/Object.cpp
+++ src/game/Object.cpp
@@ -1963,7 +1963,7 @@ void WorldObject::GetValidPointInAngle(Position &pos, float dist, float angle, b
     if (ignoreLOSOffset)
         result = VMAP::VMapFactory::createOrGetVMapManager()->getObjectHitPos(GetMapId(), pos.x, pos.y, pos.z +0.5f, dest.x, dest.y, dest.z +1.0f, dest.x, dest.y, dest.z, -0.5f);
     else
-        result = VMAP::VMapFactory::createOrGetVMapManager()->getObjectHitPos(GetMapId(), pos.x, pos.y, pos.z +3.0f, dest.x, dest.y, dest.z +7.0f, dest.x, dest.y, dest.z, -0.5f);
+        result = VMAP::VMapFactory::createOrGetVMapManager()->getObjectHitPos(GetMapId(), pos.x, pos.y, pos.z +0.5f, dest.x, dest.y, dest.z +2.0f, dest.x, dest.y, dest.z, -0.5f);
 
     if (result)
     {
