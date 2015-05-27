2f31892504bea0aa78ab3caa22ed5ce01a21fe1f
koostosh <9148800+koostosh@users.noreply.github.com>
2015-05-27 23:06:37 +0200

    lock one spell to its zone

--- src/game/SpellMgr.cpp
+++ src/game/SpellMgr.cpp
@@ -4059,6 +4059,8 @@ bool SpellMgr::IsSpellAllowedInLocation(SpellEntry const *spellInfo,uint32 map_i
 
             return mapEntry->multimap_id == 199 || mapEntry->multimap_id == 208;
         }
+        case 40817: // summon banishing portal
+            return (zone_id == 3785) || (zone_id == 3784);
     }
 
     return true;
