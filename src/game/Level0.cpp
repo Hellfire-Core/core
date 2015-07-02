9320363d83ebcd0b1b2e613c38f5532d270aa1cc
Elyrion <elyrion.hg@gmail.com>
2015-07-02 17:58:02 +0200

    Info for playars!

--- src/game/Level0.cpp
+++ src/game/Level0.cpp
@@ -53,6 +53,8 @@ bool ChatHandler::HandleAccountXPToggleCommand(const char* args)
                 session->AddAccountFlag(ACC_BLIZZLIKE_RATES);
                 PSendSysMessage("Now your rates are blizzlike: x1.");
             }
+            PSendSysMessage("");
+            PSendSysMessage("You have to RELOG before the command takes effect!");
         }
     }
     else
