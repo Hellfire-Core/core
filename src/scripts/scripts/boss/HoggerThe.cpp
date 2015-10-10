6ab60851968ae7f2a3c8c3992ac5848e5295cd53
koostosh <9148800+koostosh@users.noreply.github.com>
2015-10-10 17:52:56 +0200

    event script #2

--- src/scripts/scripts/boss/HoggerThe.cpp
+++ src/scripts/scripts/boss/HoggerThe.cpp
@@ -24,7 +24,10 @@ enum
     SPELL_PIERCE_ARMOUR	= 6016,
     SPELL_ARCANE_EXPLOSION = 37106,
 
-    NPC_IMP = 22202,
+    NPC_HOGGER          = 66716,
+    NPC_GRUFF           = 66717,
+    NPC_RIVERPAW_ELITE  = 66718,
+    NPC_IMP             = 66719,
 };
 
 struct npc_hogger_theAI : public ScriptedAI
@@ -64,34 +67,34 @@ struct npc_hogger_theAI : public ScriptedAI
         case 0:
         case 2:
         case 4:
-            SummonImps(15);
+            SummonMobs(15, NPC_IMP);
             break;
         case 1:
-        case 3:
-            SummonImps(1); // will be replaced by something more epic
+            SummonMobs(1, NPC_GRUFF);
+            SummonMobs(5, NPC_RIVERPAW_ELITE);
             break;
-        }
-
-        adsWave++;
-        spawnTimer = 3000;
-        if (adsWave == 5)
-        {
+        case 3:
+            SummonMobs(10, NPC_RIVERPAW_ELITE);
+            break;
+        case 5:
             phase = 3;
             m_creature->SetHealth(m_creature->GetMaxHealth());
             m_creature->RemoveFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_NOT_SELECTABLE);
             m_creature->RemoveFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_NON_ATTACKABLE);
             m_creature->SetVisibility(VISIBILITY_ON);
             DoCast(NULL, SPELL_ARCANE_EXPLOSION, true);
+            SummonMobs(5, NPC_RIVERPAW_ELITE);
+            return;
         }
     }
 
-    void SummonImps(uint8 count)
+    void SummonMobs(uint8 count, uint32 entry)
     {
         for (uint8 i = 0; i < count; i++)
         {
             Position pos;
             m_creature->GetValidPointInAngle(pos, frand(1.0f, 20.0f), frand(0.0f, 2 * M_PI), true);
-            if (Creature* imp = m_creature->SummonCreature(NPC_IMP, pos.x, pos.y, pos.z, pos.o, TEMPSUMMON_CORPSE_DESPAWN, 1000))
+            if (Creature* imp = m_creature->SummonCreature(entry, pos.x, pos.y, pos.z, pos.o, TEMPSUMMON_CORPSE_DESPAWN, 1000))
             {
                 summons.Summon(imp);
                 if (Unit* target = imp->SelectNearbyTarget(30.0f))
@@ -105,7 +108,7 @@ struct npc_hogger_theAI : public ScriptedAI
         summons.Despawn(spawn);
     }
 
-    void UpdateAI(uint32 diff)
+    void UpdateAI(const uint32 diff)
     {
         if (!UpdateVictim())
             return;
@@ -127,7 +130,7 @@ struct npc_hogger_theAI : public ScriptedAI
                     if (m_creature->getVictimGUID() == stunTargetGUID && target->HasAura(SPELL_HEAD_BUTT))
                         m_creature->Kill(target);
                     stunTargetGUID = 0;
-                    stunTimer = 17100;
+                    stunTimer = 7100;
                     return; // target could be dead, avoid problems
                 }
             }
@@ -135,7 +138,7 @@ struct npc_hogger_theAI : public ScriptedAI
             if (pierceTimer.Expired(diff))
             {
                 DoCast(target, SPELL_PIERCE_ARMOUR);
-                pierceTimer = 7000;
+                pierceTimer = 6000;
             }
 
             DoMeleeAttackIfReady();
@@ -160,9 +163,29 @@ struct npc_hogger_theAI : public ScriptedAI
     }
 };
 
-CreatureAI* GetAI_npc_hogger_the(Creature *_Creature)
+CreatureAI* GetAI_npc_hogger_the(Creature* c)
 {
-    return new npc_hogger_theAI(_Creature);
+    return new npc_hogger_theAI(c);
+}
+
+struct npc_gruff_ai : public ScriptedAI
+{
+    npc_gruff_ai(Creature* c) : ScriptedAI(c) {}
+
+    void IsSummonedBy(Creature* hogger)
+    {
+        m_creature->Yell("YELL SOMETHING!!!!111",0,0);
+    }
+
+    void JustDied(Unit* killer)
+    {
+        m_creature->Yell("Ouch, me died...", 0, 0);
+    }
+};
+
+CreatureAI* GetAI_npc_gruff(Creature* c)
+{
+    return new AggressorAI(c);
 }
 
 void AddSC_hogger_the()
@@ -170,7 +193,12 @@ void AddSC_hogger_the()
     Script *newscript;
 
     newscript = new Script;
-    newscript->Name = "npc_hogger_the";
+    newscript->Name = "npc_event_hogger_the";
     newscript->GetAI = &GetAI_npc_hogger_the;
+    newscript->RegisterSelf();
+
+    newscript = new Script;
+    newscript->Name = "npc_event_gruff_swiftbite";
+    newscript->GetAI = &GetAI_npc_gruff;
     newscript->RegisterSelf();
 }
