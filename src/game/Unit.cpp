1802cf147430f5cf7901318b2f092f507ae60f0e
Elyrion <elyrion.hg@gmail.com>
2015-10-25 23:35:05 +0100

    fix flask of petrification exploit

--- src/game/Unit.cpp
+++ src/game/Unit.cpp
@@ -7630,9 +7630,9 @@ bool Unit::Attack(Unit *victim, bool meleeAttack)
     }
 
     // remove SPELL_AURA_MOD_UNATTACKABLE at attack (in case non-interruptible spells stun aura applied also that not let attack)
-    if (HasAuraType(SPELL_AURA_MOD_UNATTACKABLE) && !HasAura(40282))    // do not remove Possess Spirit Immune aura when attacking
-        RemoveSpellsCausingAura(SPELL_AURA_MOD_UNATTACKABLE);
-
+    if (HasAuraType(SPELL_AURA_MOD_UNATTACKABLE) && !HasAura(40282) && !HasAura(17624, 0))    // do not remove Possess Spirit Immune aura when attacking
+        RemoveSpellsCausingAura(SPELL_AURA_MOD_UNATTACKABLE);                                 // same for Flask of petrification
+                                                                                              // TODO/FIXME: do not remove any auras that cause any form of LOST_CONTROL
     if (m_attacking)
     {
         if (m_attacking == victim)
