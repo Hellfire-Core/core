a0e890f036599b515f13088ab4eb32339ac5b455
koostosh <9148800+koostosh@users.noreply.github.com>
2016-01-25 21:55:28 +0100

    typo

--- src/game/Unit.cpp
+++ src/game/Unit.cpp
@@ -4596,7 +4596,7 @@ void Unit::RemoveAura(AuraMap::iterator &i, AuraRemoveMode mode)
                 for (int i = 0; i < 3; ++i)
                 {
                     if (AurSpellEntry->Effect[i] == SPELL_EFFECT_SUMMON &&
-                        (AurSpellEntry->EffectMiscValueB[i] == SUMMON_TYPE_POSESSED)
+                        AurSpellEntry->EffectMiscValueB[i] == SUMMON_TYPE_POSESSED)
                          //AurSpellEntry->EffectMiscValueB[i] == SUMMON_TYPE_POSESSED2 ||
                          //AurSpellEntry->EffectMiscValueB[i] == SUMMON_TYPE_POSESSED3))
                     {
