b284781585e3bc9b439c60dc021ca6f6baa7bbce
koostosh <9148800+koostosh@users.noreply.github.com>
2016-05-04 21:03:51 +0200

    crashfix

--- src/game/Creature.cpp
+++ src/game/Creature.cpp
@@ -2258,10 +2258,6 @@ bool Creature::IsOutOfThreatArea(Unit* pVictim) const
     if (!pVictim)
         return true;
 
-    // evade if owner is out of threat area
-    if (pVictim->GetCharmerOrOwnerPlayerOrPlayerItself())
-        pVictim = pVictim->GetCharmerOrOwnerPlayerOrPlayerItself();
-
     if (!pVictim->IsInMap(this))
         return true;
 
