de747c8ab0845b205cda3b144b53d410a9a76df5
koostosh <9148800+koostosh@users.noreply.github.com>
2016-05-06 17:01:22 +0200

    build fix

--- src/game/LootHandler.cpp
+++ src/game/LootHandler.cpp
@@ -365,7 +365,7 @@ void WorldSession::DoLootRelease(uint64 lguid)
         Item *pItem = player->GetItemByGuid(lguid);
         if (!pItem)
             return;
-        loot - &pItem->loot;
+        loot = &pItem->loot;
 
         ItemPrototype const* proto = pItem->GetProto();
         // destroy only 5 items from stack in case prospecting and milling
