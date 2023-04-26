a1e7eeae73c54148ba40754fd957b7b259b330cb
zz <zz@test.com>
2023-04-26 15:00:29 +0800

    adding extractors from vmangos

--- src/game/Maps/MoveMapSharedDefines.h
+++ src/game/Maps/MoveMapSharedDefines.h
@@ -45,7 +45,7 @@ enum NavTerrain
     NAV_MAGMA   = 0x02,
     NAV_SLIME   = 0x04,
     NAV_WATER   = 0x08,
-    NAV_UNUSED1 = 0x10,
+    NAV_STEEP_SLOPES = 0x10,
     NAV_UNUSED2 = 0x20,
     NAV_UNUSED3 = 0x40,
     NAV_UNUSED4 = 0x80
